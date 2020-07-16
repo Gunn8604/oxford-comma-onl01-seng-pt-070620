2ea8914e925d8395486a589d2f22036d8d2f1b78
Billy Gunn, Jr <bgunnjr@gmail.com>
2020-07-16 23:22:20 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,6 @@ def oxford_comma(array)
 elsif array.size == 1 
  return "#{array[0]}"
 elsif array.size == 3
-  return "#{array[0]}, #{array[1]} and #{array[2]}"
+  return "#{array[0]}, #{array[1]}, and #{array[2]}"
   end
 end
