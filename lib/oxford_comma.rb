2e725e462513d4fbcda670d4549e19a494756ea7
Billy Gunn, Jr <bgunnjr@gmail.com>
2020-07-16 23:38:04 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,6 +6,6 @@ elsif array.size == 1
 elsif array.size == 3
   return "#{array[0]}, #{array[1]}, and #{array[2]}"
 elsif array.size == 5
-  return "#{array[0]}, #{array[1]}, #{array[2]}, #{array[3]} and #{array[4]}"
+  return "#{array[0]}, #{array[1]}, #{array[2]}, #{array[3]}, and #{array[4]}"
 end
 end
